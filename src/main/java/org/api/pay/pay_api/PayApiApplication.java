bf350fedfdd40c0dafe6d213e3ab817e0ee9a855
youness <yrekass@gmail.com>
2025-11-13 01:15:50 +0100

    tech(): create projet

---
+++ src/main/java/org/api/pay/pay_api/PayApiApplication.java
@@ -0,0 +1,13 @@
+package org.api.pay.pay_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/api/pay/pay_api/PayApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.api.pay.pay_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
